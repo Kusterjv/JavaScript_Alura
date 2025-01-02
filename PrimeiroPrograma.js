2cbf4cfc794a8acfec4fe8f76e05de16179f9b57
João Vitor Küster <105330560+Kusterjv@users.noreply.github.com>
2025-01-01 21:20:39 -0300

    Adicionando arquivos de estudo em JavaScript

---
+++ PrimeiroPrograma.js
@@ -0,0 +1,14 @@
+// testando alguns parâmetros da linguagem
+console.log("Meu primeiro programa?: Trabalhando com variáveis");
+
+const idade = 24;
+
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade * 2);
+console.log(idade / 2);
+
+
+const somaIdade = idade + 3;
+console.log(somaIdade);
